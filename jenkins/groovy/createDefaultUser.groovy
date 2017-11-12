cc0a76781f20e24af10371a55face515382a3eb2
ylacaute <yannick.lacaute@gmail.com>
2017-11-12 21:57:29 +0100

    Add build status plugin, add anonymous permission to see build status

--- jenkins/groovy/createDefaultUser.groovy
+++ jenkins/groovy/createDefaultUser.groovy
@@ -1,14 +1,38 @@
 import hudson.security.*
 import jenkins.model.*
 
+// JVM did not like 'hypen' in the class name, it will crap out saying it is illegal class name.
+class BuildPermission {
+  static buildNewAccessList(userOrGroup, permissions) {
+    def newPermissionsMap = [:]
+    permissions.each {
+      newPermissionsMap.put(Permission.fromId(it), userOrGroup)
+    }
+    return newPermissionsMap
+  }
+}
 def instance = Jenkins.getInstance()
-
 def hudsonRealm = new HudsonPrivateSecurityRealm(false, false, null)
+
+// Create admin account
 def credentials = new File("/run/secrets/admin").text.trim().split(":")
 hudsonRealm.createAccount(credentials[0], credentials[1])
 instance.setSecurityRealm(hudsonRealm)
 
+// Security strategie
 def strategy = new GlobalMatrixAuthorizationStrategy()
+
+// Add admin account
 strategy.add(Jenkins.ADMINISTER, credentials[0])
+
+// Set anonymous permissions
+anonymousPermissions = [
+ //"hudson.model.Hudson.Read",
+ //"hudson.model.Item.Read",
+ "hudson.model.Item.ViewStatus"
+]
+anonymous = BuildPermission.buildNewAccessList("anonymous", anonymousPermissions)
+anonymous.each { p, u -> strategy.add(p, u) }
+
 instance.setAuthorizationStrategy(strategy)
 instance.save()
